074278467bfd4c43270b8882a588f85fd26aa852
Alexey <maracka@bk.ru>
2022-03-17 14:26:16 +0300

    tri sposoba zadaniya cikla

--- js/script.js
+++ js/script.js
@@ -9,9 +9,42 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+// Первый способ задания цикла
+// let i = 0;
 
+// do {
 
+//     const a = prompt("Один из последних просмотренных фильмов", ""),
+//     b = +prompt("На сколько оцените его", "");
 
+// if (a != null && b!= null && a != '' && b!= '' && a.length < 50){
+//   personalMovieDB.movies[a] = b;
+//   console.log ('done');
+// }else {
+//   console.log('error');
+//   i--;
+// }
+// i++
+// }
+// while(i < 2);
+
+//ВТОРОЙ СПОСОБ ЗАДАНИЯ ЦИКЛА
+// let i =0;
+
+// while(i<2){
+//     const a = prompt("Один из последних просмотренных фильмов", ""),
+//           b = +prompt("На сколько оцените его", "");
+//     if (a != null && b!= null && a != '' && b!= '' && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//         console.log ('done');
+//     }else {
+//         console.log('error');
+//         i--;
+//     }
+//     i++
+// }
+
+// ТРЕТИЙ СПОСОБ ЗАДАНИЯ ЦИКЛА
 for(let i = 0; i < 2; i++){
     const a = prompt("Один из последних просмотренных фильмов", ""),
           b = +prompt("На сколько оцените его", "");
@@ -34,3 +67,5 @@ if (personalMovieDB.count < 10){
 } else {
     console.log('Произошла ошибка');
 }
+
+console.log (personalMovieDB);
